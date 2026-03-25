c3f4a7f5e893cadec5fa2723df98eff68b1641a2
sneha.d <sneha.d@indianic.com>
2026-03-25 11:42:03 +0530

    Fix syntax and import errors in taskRoutes

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
-const auth = require('../middleware/auth');
+const { protect } = require('../middleware/authMiddleware');
 
 // All task routes are protected
-router.post('/', auth, taskController.createTask);
-router.get('/', auth, taskController.getTasks);
-router.patch('/:id/toggle', auth, taskController.toggleComplete);
-router.put('/:id', auth, taskController.updateTask);
-router.delete('/:id', auth, taskController.deleteTask);
+router.post('/', protect, taskController.createTask);
+router.get('/', protect, taskController.getTasks);
+router.patch('/:id/toggle', protect, taskController.toggleComplete);
+router.put('/:id', protect, taskController.updateTask);
+router.delete('/:id', protect, taskController.deleteTask);
 
 // Public Link for One-Click Rescheduling (Inbox Actions)
 router.get('/:id/reschedule/:days', taskController.rescheduleTask);
